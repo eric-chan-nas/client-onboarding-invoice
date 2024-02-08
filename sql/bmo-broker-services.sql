8b2fe960f34219f485b23ea189c571422d17ab44
AzureAD\EricChan <eric.chan@tngoc.com>
2024-02-08 17:20:30 -0500

    NE-11985

---
+++ sql/bmo-broker-services.sql
@@ -0,0 +1,36 @@
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'APP_REQUEST_STATUS', 'Completed,Reviewed,NAS_Reviewed,Cancelled,Under_Review,NAS_Under_Review,Resubmitted,OHC,OHA,OHR', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'BILLING_DAY_OF_WEEK', 'MONDAY thru FRIDAY', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'BILLING_FREQUENCY', 'DAILY', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'COMPANY', 'Bank of Montreal Broker Services', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'FINANCIAL_STATUS', 'TB', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'INCLUDE_ZERO_VALUES', 'TRUE', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'INVOICE_EXTENSION', 'xls', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'MARK_INVOICED', 'TRUE', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'PAYMENT_TYPES', 'N,P', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'PROVINCE', 'ALL', NULL);
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'TB_INVOICED_DATE_RANGE', '2024-02-01 00:00:00', '2024-03-01 00:00:00');
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Bank of Montreal Broker Services', 'TEMPLATE_NAME', 'generateCustomInvoice', NULL);
